0d5477c58af187ace3f1a723a47ee321b09b8e3e
Toxictro <44504926+Toxictro@users.noreply.github.com>
2019-03-19 23:37:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "!";
 client.on('ready', ()=>{
   console.log(`Logged in as ${client.user.tag}! Welcome Back`);
-  client.user.setActivity("Twitch.tv/asdrtyuip", {type: 1});
+  client.user.setActivity("Twitch.tv/asdrtyuip", {type: 3});
 });
 
 client.on('guildMemberAdd', member => {
